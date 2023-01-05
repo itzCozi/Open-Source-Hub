4e6761723e30caaf785929f74635783ca85e85a0
cozi <93058744+itzCozi@users.noreply.github.com>
2023-01-05 18:23:33 -0500

    Contribution
    Contribution

---
+++ Name-checker/Fork_Windows/install.bat
@@ -0,0 +1,9 @@
+rem Sorry for bad batch coding learned it just for this
+@echo on
+
+rem Install packages through pip
+pip install requests
+pip install colorama
+
+echo "Install done "
+PAUSE
---
+++ Name-checker/main.py
@@ -0,0 +1,5 @@
+# https://github.com/useragents/Proxyless-TikTok-Username-Checker
+# TODO: Upload directorys and files into this repo for fork destination and pull-request: (https://github.com/itzCozi/Open-Source-Workspace)
+# TODO: Test windows compatiblity on home pc, reformat all code
+
+from Fork_Universal import start
